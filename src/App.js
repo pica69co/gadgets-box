f87aa54f41da8be254d044614754bdac19b3fa22
pica69co <oscarwarrieta@gmail.com>
2023-08-29 14:59:26 -0500

    music-player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Quotes from './pages/Quotes';
-import Weather from './pages/Weather';
+import MusicPlayer from './pages/MusicPlayer/MusicPlayer';
+// import Quotes from './pages/Quotes';
+// import Weather from './pages/Weather';
 
 function App() {
   return (
     <div className="App">
       <h1>Gadgets Box</h1>
       {/* <Quotes /> */}
-      <Weather />
+      {/* <Weather /> */}
+      <MusicPlayer />
     </div>
   );
 }
