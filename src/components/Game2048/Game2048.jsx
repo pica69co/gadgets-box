4830bd4ebf3ac74e8c843342d4b29eb1eaf95130
pica69co <oscarwarrieta@gmail.com>
2023-08-31 11:22:48 -0500

    undercontruction link

--- src/components/Game2048/Game2048.jsx
+++ src/components/Game2048/Game2048.jsx
@@ -7,6 +7,7 @@ export const Game2048 = () => {
         <div>
           <h1>Play 2048</h1>
           <p>Under construction..</p>
+          <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  />
         </div>
         
     </div>
--- src/components/MemoryGame/MemoryGame.jsx
+++ src/components/MemoryGame/MemoryGame.jsx
@@ -5,6 +5,7 @@ const MemoryGame = () => {
     <div>
     <h1>Play MemoryGame</h1>
     <p>Under Construction...</p>
+    <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  />
     </div>
   )
 }
