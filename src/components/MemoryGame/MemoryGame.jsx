0558a81b9b41b9b22cd29a2665a5af13c3750d34
pica69co <oscarwarrieta@gmail.com>
2023-09-01 10:25:25 -0500

    firstsprint

--- src/components/MemoryGame/MemoryGame.jsx
+++ src/components/MemoryGame/MemoryGame.jsx
@@ -14,6 +14,8 @@ const MemoryGame = () => {
     <h1>Play MemoryGame</h1>
      <Background />
      <Settings />
+     <p>Under Construction...</p>
+    <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  />
     </div>
   )
 }
