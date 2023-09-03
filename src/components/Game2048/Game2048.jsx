3779d34857278eef1794f60a64d398970a9fe296
pica69co <oscarwarrieta@gmail.com>
2023-09-02 19:28:14 -0500

    init2048

--- src/components/Game2048/Game2048.jsx
+++ src/components/Game2048/Game2048.jsx
@@ -1,15 +1,37 @@
-
+import './Game2048.css'
 
 export const Game2048 = () => {
       
   return(
     <div className="header">
+        <p>Under construction..</p>
+          <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  width='500'/>
         <div>
           <h1>Play 2048</h1>
-          <p>Under construction..</p>
-          <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  />
+          
+          <div className='game-board' id='game-board'>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>
+          <div className='cell'></div>  
+          
+          <div className='tile'>2</div>
+          </div>
+
         </div>
-        
+
     </div>
   )
 }
