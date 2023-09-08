36cfda45e111fd1444bc36f4db54256c3e662214
pica69co <oscarwarrieta@gmail.com>
2023-09-08 15:02:36 -0500

    tetris-cellcolor

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -5,13 +5,6 @@ import { useTetris } from './Hooks/useTetris';
 import '../Tetris/Tetris.css'
 
 const Tetris = () => {
-  // return (
-  //   <div>
-  //   <h1>Play Tetris</h1>
-  //   <p>Under Construction...</p>
-  //   <img src="https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg" alt="icon"  />
-  //   </div>
-  // )
   const { board, startGame, isPlaying, score, upcomingBlocks } = useTetris();
   
   return (
