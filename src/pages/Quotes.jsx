42ac995c1ed8c091246df8f0cef536dca3fbf45e
pica69co <oscarwarrieta@gmail.com>
2023-09-08 21:05:58 -0500

    quotes-clipboard

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -1,78 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import xIcon from '../assets/images/twitter-x-icon.png'
-// import meSpeak from 'mespeak'
-// import voices from 'mespeak/voices/en/en-us.json'
+import React, { useEffect, useState } from "react";
+import xIcon from "../assets/images/twitter-x-icon.png";
+
 const Quotes = () => {
-  
-    const [quote, setQuote] = useState('')
-    const [author, setAuthor] = useState('')
-    const api_url = 'https://api.quotable.io/random'
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [copied, setCopied] = useState("");
+  const api_url = "https://api.quotable.io/random";
 
-    const fetchQuotes = async () => {
-        const response = await fetch(api_url)
-        const data = await response.json()
-        setQuote(data.content)
-        setAuthor(data.author)
+  const fetchQuotes = async () => {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
 
-        console.log(data)
-    }
-    
-    useEffect(() => {
-       fetchQuotes()
+    console.log(data);
+  };
 
-           
-     }, [])
-   
-        
-     const handlerQuote = () => {
-    fetchQuotes()
-   }
-   
-   const handleClickTweet = () => {
-    window.open(`https://twitter.com/intent/tweet?text=${quote} --- by ${author}`, "Tweet Window", "width=800, height=500")
-   }
-   
-   const handlerSound =  () => {
-     
-     const utterance = new SpeechSynthesisUtterance(`${quote},  ${author}` )
-     const voices =  speechSynthesis?.getVoices()
-     
-     utterance.voice = voices[3] || voices[2]         
-      
-       let voiceName = utterance.voice?.name
-       console.log('voz',voiceName);
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const handlerQuote = () => {
+    fetchQuotes();
+  };
+
+  const handleClickTweet = () => {
+    window.open(
+      `https://twitter.com/intent/tweet?text=${quote} --- by ${author}`,
+      "Tweet Window",
+      "width=800, height=500"
+    );
+  };
+
+  const handlerSound = () => {
+    const utterance = new SpeechSynthesisUtterance(`${quote},  ${author}`);
+    const voices = speechSynthesis?.getVoices();
+
+    utterance.voice = voices[3] || voices[2];
+
+    let voiceName = utterance.voice?.name;
+    console.log("voz", voiceName);
     //   // utterance.rate = 0.9
-       speechSynthesis.speak(utterance)
-   }
-  
-   return (
-    <div className='quote-box'>
+    speechSynthesis.speak(utterance);
+  };
+
+  const handlerCopy = (e) => {
+    e.preventDefault();
+    setCopied(`${quote} -- ${author}`);
+
+    navigator.clipboard.writeText(`${quote} -- ${author}`);
+
+    setTimeout(() => setCopied(false), 5000);
+  };
+  console.log("copy:", copied);
+  return (
+    <div className="quote-box">
       <h2>Quote of the day</h2>
       <blockquote>{quote}</blockquote>
       <span>{author}</span>
       <div>
-       <button
-        onClick={handlerQuote}
-        >New Quote
+        <button onClick={handlerQuote}>
+          {quote ? "New Quote" : "Loading..."}
         </button>
-        <button
-         onClick={handleClickTweet}
-        ><img src={xIcon} alt="tweeter-icon" />
-            Tweet
+        <button onClick={handleClickTweet}>
+          <img src={xIcon} alt="tweeter-icon" />
+          Tweet
         </button>
-        <button
-        onClick={handlerQuote}
-        className='sound'
-        > <i className='fas fa-copy copy'></i>
+        <button onClick={handlerCopy} className="copy">
+          {" "}
+          <i
+            className={
+              copied === `${quote} -- ${author}`
+                ? "fa-solid fa-check-double copy"
+                : "fas fa-copy"
+            }
+          ></i>
         </button>
-        <button
-        onClick={handlerSound}
-        className='copy'
-        ><i className='fas fa-volume-up sound' ></i>
+        <button onClick={handlerSound} className="sound">
+          <i className="fas fa-volume-up sound"></i>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Quotes
+export default Quotes;
