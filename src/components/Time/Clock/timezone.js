85dfe2ff61f8bb0d3eebc1e0f279e46e0a4deed1
pica69co <oscarwarrieta@gmail.com>
2023-09-16 10:55:55 -0500

    urlflag

--- src/components/Time/Clock/timezone.js
+++ src/components/Time/Clock/timezone.js
@@ -4,322 +4,414 @@ export const timeZone = [
       "city": "New York",
       "country": "USA",
       "gmt": "-4",
-      "utc": "-4"
+      "utc": "-4",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/London",
       "city": "London",
       "country": "United Kingdom",
       "gmt": "+1",
-      "utc": "0"
+      "utc": "0",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Tokyo",
       "city": "Tokyo",
       "country": "Japan",
       "gmt": "+9",
-      "utc": "+9"
+      "utc": "+9",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Australia/Sydney",
       "city": "Sidney",
       "country": "Australia",
       "gmt": "+10",
-      "utc": "+10"
+      "utc": "+10",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Los_Angeles",
       "city": "Los Angeles",
       "country": "USA",
       "gmt": "-7",
-      "utc": "-7"
+      "utc": "-7",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Paris",
       "city": "Paris",
       "country": "France",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Shanghai",
       "city": "Shanghai",
       "country": "China",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Johannesburg",
       "city": "Johannesburg",
       "country": "South Africa",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Toronto",
       "city": "Toronto",
       "country": "Canada",
       "gmt": "-4",
-      "utc": "-4"
+      "utc": "-4",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Berlin",
       "city": "Berlin",
       "country": "Germany",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Dubai",
       "city": "Dubai",
       "country": "United Arab Emirates",
       "gmt": "+4",
-      "utc": "+4"
+      "utc": "+4",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Pacific/Auckland",
       "city": "Auckland",
       "country": "New Zealand",
       "gmt": "+12",
-      "utc": "+12"
+      "utc": "+12",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Mexico_City",
       "city": "Mexico City",
       "country": "Mexico",
       "gmt": "-5",
-      "utc": "-5"
+      "utc": "-5",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Moscow",
       "city": "Moscow",
       "country": "Russia",
       "gmt": "+3",
-      "utc": "+3"
+      "utc": "+3",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Singapore",
       "city": "Singapore",
       "country": "Singapore",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Cairo",
       "city": "Cairo",
       "country": "Egypt",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Sao_Paulo",
       "city": "Sao Paulo",
       "country": "Brasil",
       "gmt": "-3",
-      "utc": "-3"
+      "utc": "-3",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Kolkata",
       "city": "New Delhi",
       "country": "India",
       "gmt": "+5:30",
-      "utc": "+5:30"
+      "utc": "+5:30",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Australia/Melbourne",
       "city": "Melbourne",
       "country": "Australia",
       "gmt": "+10",
-      "utc": "+10"
+      "utc": "+10",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Bogota",
       "city": "Bogota",
       "country": "Colombia",
       "gmt": "-5",
-      "utc": "-5"
+      "utc": "-5",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Madrid",
       "city": "Madrid",
       "country": "Spain",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Jakarta",
       "city": "Jakarta",
       "country": "Indonesia",
       "gmt": "+7",
-      "utc": "+7"
+      "utc": "+7",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Lagos",
       "city": "Lagos",
       "country": "Nigeria",
       "gmt": "+1",
-      "utc": "+1"
+      "utc": "+1",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Buenos_Aires",
       "city": "Buenos Aires",
       "country": "Argentina",
       "gmt": "-3",
-      "utc": "-3"
+      "utc": "-3",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Seoul",
       "city": "Seoul",
       "country": "South Korea",
       "gmt": "+9",
-      "utc": "+9"
+      "utc": "+9",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Australia/Brisbane",
       "city": "Brisbane",
       "country": "Australia",
       "gmt": "+10",
-      "utc": "+10"
+      "utc": "+10",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Vancouver",
       "city": "Vancouver",
       "country": "Canada",
       "gmt": "-7",
-      "utc": "-7"
+      "utc": "-7",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Stockholm",
       "city": "Stockholm",
       "country": "Sweden",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Riyadh",
       "city": "Riyadh",
       "country": "Saudi Arabia",
       "gmt": "+3",
-      "utc": "+3"
+      "utc": "+3",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Nairobi",
       "city": "Nairobi",
       "country": "Kenya",
       "gmt": "+3",
-      "utc": "+3"
+      "utc": "+3",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Denver",
       "city": "Denver",
       "country": "USA",
       "gmt": "-6",
-      "utc": "-6"
+      "utc": "-6",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Amsterdam",
       "city": "Amsterdam",
       "country": "Netherlands",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Manila",
       "city": "Manila",
       "country": "Philippines",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Casablanca",
       "city": "Casablanca",
       "country": "Morocco",
       "gmt": "+1",
-      "utc": "+1"
+      "utc": "+1",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Phoenix",
       "city": "Phoenix",
       "country": "USA",
       "gmt": "-7",
-      "utc": "-7"
+      "utc": "-7",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Prague",
       "city": "Prague",
       "country": "Czech Republic",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Taipei",
       "city": "Taipei",
       "country": "Taiwan",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Tunis",
       "city": "Tunisia",
       "country": "Tunisia",
       "gmt": "+1",
-      "utc": "+1"
+      "utc": "+1",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Atlanta",
       "city": "Atlanta",
       "country": "USA",
       "gmt": "-4",
-      "utc": "-4"
+      "utc": "-4",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Vienna",
       "city": "Vienna",
       "country": "Austria",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Kuala_Lumpur",
       "city": "Kuala Lumpur",
       "country": "Malaysia",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Algiers",
       "city": "Algiers",
       "country": "Algeria",
       "gmt": "+1",
-      "utc": "+1"
+      "utc": "+1",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "America/Detroit",
       "city": "Detroit",
       "country": "USA",
       "gmt": "-4",
-      "utc": "-4"
+      "utc": "-4",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Europe/Warsaw",
       "city": "Warsaw",
       "country": "Poland",
       "gmt": "+2",
-      "utc": "+2"
+      "utc": "+2",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Asia/Hong_Kong",
       "city": "Hong Kong",
       "country": "Hong Kong",
       "gmt": "+8",
-      "utc": "+8"
+      "utc": "+8",
+      "urlFlag":"",
+      "lang":"",
     },
     {
       "timezone": "Africa/Luanda",
       "city": "Luanda",
       "country": "Angola",
       "gmt": "+1",
-      "utc": "+1"
+      "utc": "+1",
+      "urlFlag":"",
+      "lang":"",
     }
   ]
   
