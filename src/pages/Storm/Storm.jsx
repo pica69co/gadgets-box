40deabe9cc4a1bcb82b1dd65b025032c2016efdc
pica69co <oscarwarrieta@gmail.com>
2023-09-16 10:59:05 -0500

    stormUc

--- src/pages/Storm/Storm.jsx
+++ src/pages/Storm/Storm.jsx
@@ -4,7 +4,7 @@ const Storm = () => {
   return (
     <div>
         <h1>Storm-Tracking </h1>
-        
+        <img src='https://cdn.pixabay.com/photo/2016/03/26/09/42/road-sign-1280257_960_720.jpg' alt='background'/>
     </div>
   )
 }
