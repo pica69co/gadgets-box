6e5a843f1143d0db580518ab3144f8f65bdfb55e
pica69co <oscarwarrieta@gmail.com>
2023-09-17 10:20:29 -0500

    style atlantic

--- src/pages/Storm/Views/Atlantic.jsx
+++ src/pages/Storm/Views/Atlantic.jsx
@@ -8,21 +8,25 @@ const Atlantic = () => {
   return (
     <div className='atlantic'>
       <div>
-        <h1>Conditions in the Atlantic Ocean</h1>
+        <h1 className='atlantic-title'>Conditions in the Atlantic Ocean</h1>
         <div className='atlantic-list'>
           <div className='list-item'>
               <h1>{filtered[0].desc}</h1>
-              <img src={filtered[0].url} alt='atlantic' width={550} height={320} />
+              <img src={filtered[0].url} alt='atlantic' width={300} height={150} />
           </div>
           
           {/*ToDo: component GalleryImages */}
           <div className='list-item'>
               <h1>{filtered[0].desc2}</h1>
-              <img src={filtered[0].url2} alt='atlantic' width={550} height={320} />
+              <img src={filtered[0].url2} alt='atlantic2' width={300} height={150} />
           </div>
           <div className='list-item'>
               <h1>{filtered[0].desc3}</h1>
-              <img src={filtered[0].url3} alt='atlantic' width={550} height={320} />
+              <img src={filtered[0].url3} alt='atlantic3' width={300} height={150} />
+          </div>
+          <div className='list-item'>
+              <h1>{filtered[0].desc4}</h1>
+              <img src={filtered[0].url4} alt='atlantic4' width={350} height={150} />
           </div>
         </div>
 
