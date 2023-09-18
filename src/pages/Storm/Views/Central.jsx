285cc829c990d41747e01abf2be1a7da2a2f9501
pica69co <oscarwarrieta@gmail.com>
2023-09-18 11:07:51 -0500

    feat cantralamerica

--- src/pages/Storm/Views/Central.jsx
+++ src/pages/Storm/Views/Central.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { storm } from "../storm";
 
 const Central = () => {
-  return (
-    <div>Central</div>
-  )
-}
+  const filtered = storm.filter(
+    (s) => s.name === "Conditions in Central America"
+  );
 
-export default Central
+  return (
+    <div className="central">
+      <div>
+        <h1 className="central-title">Conditions in the Central America</h1>
+        <div className="central-list">
+          <div className="list-item">
+            <h1>{filtered[0].desc}</h1>
+            <img
+              src={filtered[0].url}
+              alt="central"
+              width={350}
+              height={150}
+              title=""
+            />
+          </div>
+
+          {/*ToDo: component GalleryImages */}
+
+          <div className="list-item">
+            <h1>{filtered[0].desc2}</h1>
+            <img
+              src={filtered[0].url2}
+              alt="central2"
+              width={350}
+              height={150}
+              title=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Central;
