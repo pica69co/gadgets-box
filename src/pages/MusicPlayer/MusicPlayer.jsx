eeed88338d184d18390eafa47d4ad9cd893ad67a
pica69co <oscarwarrieta@gmail.com>
2023-09-25 09:54:11 -0500

    musicplayer styles

--- src/pages/MusicPlayer/MusicPlayer.jsx
+++ src/pages/MusicPlayer/MusicPlayer.jsx
@@ -1,134 +1,28 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { musicAPI } from './music';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import './MusicPlayer.css';
 
 function MusicPlayer() {
 
-  const [currentMusicDetails, setCurrentMusicDetails] = useState({
-    songName: 'Chasing',
-    songArtist: 'NEFFEX',
-    songSrc: './assets/audio/Chasing - NEFFEX.mp3',
-    songAvatar: './assets/images/image1.jpg'
-  })
+  //  audioplayer
+  const [trackIndex, setTrackIndex] = useState(0);
 
-  //UseStates Variables
-  const [audioProgress, setAudioProgress] = useState(0);
-  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
-  const [musicIndex, setMusicIndex] = useState(0);
-  const [musicTotalLength, setMusicTotalLength] = useState('04 : 38');
-  const [musicCurrentTime, setMusicCurrentTime] = useState('00 : 00');
-  const [videoIndex, setVideoIndex] = useState(0)
+  const handleClickPrevious = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack === 0 ? musicAPI.length - 1 : currentTrack - 1
+    );
+  };
 
-  const currentAudio = useRef()
+  const handleClickNext = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < musicAPI.length - 1 ? currentTrack + 1 : 0
+    );
+  };
 
-  const handleMusicProgressBar = (e)=>{
-    setAudioProgress(e.target.value);
-    currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
-  }
-
-  //Change Avatar Class
-  let avatarClass = ['objectFitCover','objectFitContain','none']
-  const [avatarClassIndex, setAvatarClassIndex] = useState(0)
-  const handleAvatar = ()=>{
-    if (avatarClassIndex >= avatarClass.length - 1) {
-      setAvatarClassIndex(0)
-    }else{
-      setAvatarClassIndex(avatarClassIndex + 1)
-    }
-  }
-
-
-  //Play Audio Function
-  const handleAudioPlay = ()=>{
-    if (currentAudio.current.paused) {
-      currentAudio.current.play();
-      setIsAudioPlaying(true)
-    }else{
-      currentAudio.current.pause();
-      setIsAudioPlaying(false)
-    }
-  }
-  //*ToDo: music-play list from internet.
-  const musicAPI = [
-    {
-      songName: 'Chasing',
-      songArtist: 'NEFFEX',
-      songSrc: './assets/audio/Chasing - NEFFEX.mp3',
-      songAvatar: './assets/images/image1.jpg'
-    },
-    {
-      songName: 'AURORA - Runaway',
-      songArtist: 'Aurora Aksnes',
-      songSrc: './assets/audio/AURORA - Runaway (Lyrics).mp3',
-      songAvatar: './assets/images/image1.jpg'
-    },
-    
-  ]
-
-  const handleNextSong = ()=>{
-    if (musicIndex >= musicAPI.length - 1) {
-      let setNumber = 0;
-      setMusicIndex(setNumber);
-      updateCurrentMusicDetails(setNumber);
-    }else{
-      let setNumber = musicIndex + 1;
-      setMusicIndex(setNumber)
-      updateCurrentMusicDetails(setNumber);
-    }
-  }
-
-  const handlePrevSong = ()=>{
-    if (musicIndex === 0) {
-      let setNumber = musicAPI.length - 1;
-      setMusicIndex(setNumber);
-      updateCurrentMusicDetails(setNumber);
-    }else{
-      let setNumber = musicIndex - 1;
-      setMusicIndex(setNumber)
-      updateCurrentMusicDetails(setNumber);
-    }
-  }
-
-  const updateCurrentMusicDetails = (number)=>{
-    let musicObject = musicAPI[number];
-    currentAudio.current.src = musicObject.songSrc;
-    currentAudio.current.play();
-    setCurrentMusicDetails({
-      songName: musicObject.songName,
-      songArtist: musicObject.songArtist,
-      songSrc: musicObject.songSrc,
-      songAvatar: musicObject.songAvatar
-    })
-    setIsAudioPlaying(true);
-  }
-
-  const handleAudioUpdate = ()=>{
-    //Input total length of the audio
-    let minutes = Math.floor(currentAudio.current.duration / 60);
-    let seconds = Math.floor(currentAudio.current.duration % 60);
-    let musicTotalLength0 = `${minutes <10 ? `0${minutes}` : minutes} : ${seconds <10 ? `0${seconds}` : seconds}`;
-    setMusicTotalLength(musicTotalLength0);
-
-    //Input Music Current Time
-    let currentMin = Math.floor(currentAudio.current.currentTime / 60);
-    let currentSec = Math.floor(currentAudio.current.currentTime % 60);
-    let musicCurrentT = `${currentMin <10 ? `0${currentMin}` : currentMin} : ${currentSec <10 ? `0${currentSec}` : currentSec}`;
-    setMusicCurrentTime(musicCurrentT);
-
-    const progress = parseInt((currentAudio.current.currentTime / currentAudio.current.duration) * 100);
-    setAudioProgress(isNaN(progress)? 0 : progress)
-  }
-
-
-  const vidArray = ['background1','background2','background3','background4','background5','background6','background7'];
-
-  const handleChangeBackground = ()=>{
-    if (videoIndex >= vidArray.length - 1) {
-      setVideoIndex(0);
-    }else{
-      setVideoIndex(videoIndex + 1)
-    }
-  }
+  //Todo: add playlist functionality
 
 
   return (
@@ -137,32 +31,26 @@ function MusicPlayer() {
       animate={{width:'100%'}}
       exit={{x:window.innerWidth, transition:{duration:0.1}}}
     >
-    <div className={`${vidArray[videoIndex]} container` } >
-      <audio src='./assets/audio/Chasing - NEFFEX.mp3' ref={currentAudio} onEnded={handleNextSong} onTimeUpdate={handleAudioUpdate}></audio>
-      
-      <div className={`'${vidArray[videoIndex]}'`}></div>
-      <div className="music-Container">
-        <p className='musicPlayer'>Music Player 
-          <img src='./icons/head.svg' alt='headphone-icon' className='headphone'/></p>
-        <p className='music-Head-Name'>{currentMusicDetails.songName}</p>
-        <p className='music-Artist-Name'>{currentMusicDetails.songArtist}</p>
-        <img src={currentMusicDetails.songAvatar} className={avatarClass[avatarClassIndex]} style={{width:'80px', height:'80px'}} onClick={handleAvatar} alt="song Avatar" id='songAvatar'/>
+    <div className= 'container'  >
+      <div className="music-container">
+
+      <AudioPlayer
+        // style={{ width: "300px" }}
+        style={{ borderRadius: "1rem", color:'black', fontSize:'1rem', width:'100%', height:'100%' }}
+        autoPlay
+        //layout="horizontal"
+        src={musicAPI[trackIndex].src}
+        onPlay={(e) => console.log("onPlay")}
+        showSkipControls={true}
+        showJumpControls={false}
+        header={`Now playing: ${musicAPI[trackIndex].name}`}
+        footer="Music from: www.bensound.com"
+        onClickPrevious={handleClickPrevious}
+        onClickNext={handleClickNext}
+        onEnded={handleClickNext}
         
-        
-        <div className="musicControllers">
-          <i className='fa-solid fa-backward musicController' onClick={handlePrevSong}></i>
-          <i className={`fa-solid ${isAudioPlaying? 'fa-pause-circle' : 'fa-circle-play'} playBtn`} onClick={handleAudioPlay}></i>
-          <i className='fa-solid fa-forward musicController' onClick={handleNextSong}></i>
+        />
         </div>
-        <div className="musicTimerDiv">
-          <p className='musicCurrentTime'>{musicCurrentTime}</p>
-          <p className='musicTotalLength'>{musicTotalLength}</p>
-        </div>
-        <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar} />
-      </div>
-      <div className="changeBackBtn" onClick={handleChangeBackground}>
-        
-      </div>
     </div>
     </motion.div>
   );
