2f79a7913d9efc88efccd9319ed835b15e02c89c
pica69co <oscarwarrieta@gmail.com>
2023-09-25 09:54:39 -0500

    musicapi

---
+++ src/pages/MusicPlayer/music.js
@@ -0,0 +1,97 @@
+export const musicAPI = [
+    {
+      name: 'Chasing',
+      songArtist: 'NEFFEX',
+      src: './assets/audio/Chasing - NEFFEX.mp3',
+      songAvatar: './assets/images/image1.jpg'
+    },
+    {
+      name: 'AURORA - Runaway',
+      songArtist: 'Aurora Aksnes',
+      src: './assets/audio/AURORA - Runaway (Lyrics).mp3',
+      songAvatar: './assets/images/image1.jpg'
+    },
+    {
+      name: "Memories",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-memories.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Creative Minds",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-creativeminds.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Acoustic Breeze",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-acousticbreeze.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Sunny",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-sunny.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Tenderness",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-tenderness.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Once Again",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-onceagain.mp3",
+      songAvatar:'',
+    },
+    {
+      name: "Sweet",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-sweet.mp3"
+    },
+    {
+      name: "Love",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-love.mp3"
+    },
+    {
+      name: "Piano Moment",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-pianomoment.mp3"
+    },
+    {
+      name: "E.R.F",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-erf.mp3"
+    },
+    {
+      name: "Dreams",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-dreams.mp3"
+    },
+    {
+      name: "A Day To Remember",
+      songArtist: '',
+      src:
+        "https://www.bensound.com/royalty-free-music/track/a-day-to-remember-wedding-music"
+    },
+    {
+      name: "Adventure",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-adventure.mp3"
+    },
+    {
+      name: "Photo Album",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-photoalbum.mp3"
+    },
+    {
+      name: "November",
+      songArtist: '',
+      src: "https://www.bensound.com/bensound-music/bensound-november.mp3"
+    }
+    
+  ]
