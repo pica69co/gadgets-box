30d9e8bc1298c2cd7948e7049f462c39e0303c90
pica69co <oscarwarrieta@gmail.com>
2023-10-15 12:08:38 -0500

    icon link for webcams

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './VideoPlayer.css'
+import "./VideoPlayer.css";
 
 const VideoPlayer = ({ camDetails, modal, setModal, loading, setLoading }) => {
   console.log("webcams details", camDetails);
@@ -13,15 +13,15 @@ const VideoPlayer = ({ camDetails, modal, setModal, loading, setLoading }) => {
       ) : (
         <div className="video-player">
           <div className="cam-detail">
-            <a 
-            className="link"
-            href={camDetails?.player.day} rel="noreferrer noopener">
-              {camDetails.title}{" "}
+            <a
+              className="link"
+              href={camDetails?.player.day}
+              rel="noreferrer noopener"
+            >
+              {camDetails?.title}{" "}
             </a>
           </div>
-          <button 
-          onClick={() => setModal(false)} className="button-modal"
-          >
+          <button onClick={() => setModal(false)} className="button-modal">
             Close
           </button>
         </div>
--- src/pages/Storm/Views/Webcams.jsx
+++ src/pages/Storm/Views/Webcams.jsx
@@ -1,63 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { FetchWebcams, FetchWebcamsById } from '../webcams'
-import VideoPlayer from '../../../components/VideoPlayer/VideoPlayer'
-import '../Storm.css'
+import React, { useEffect, useState } from "react";
+import { FetchWebcams, FetchWebcamsById } from "../webcams";
+import VideoPlayer from "../../../components/VideoPlayer/VideoPlayer";
+import link from "../../../assets/images/link-svgrepo-com.svg";
+import "../Storm.css";
 
 const Webcams = () => {
-  const [webcams, setWebcams] = useState(null)
-  const [camDetails, setCamDetails] = useState(null)
-  
-  const [modal, setModal] = useState(false)
-  const [loading, setLoading] = useState(false)
-  
+  const [webcams, setWebcams] = useState(null);
+  const [camDetails, setCamDetails] = useState(null);
+
+  const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    setLoading(true)
-    FetchWebcams().then((data) => setWebcams(data))
-    setLoading(false)
-  },[])
-  
+    setLoading(true);
+    FetchWebcams().then((data) => setWebcams(data));
+    setLoading(false);
+  }, []);
 
-  const handlerClick = (e) =>  {
-    const webcamId = e.target.id  
-    setLoading(true)
-    setModal(true)
+  const handlerClick = (e) => {
+    const webcamId = e.target.id;
+    setLoading(true);
+    setModal(true);
+
     FetchWebcamsById(webcamId)
-    .then((data) => {
-      setCamDetails(data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-    setLoading(false)
-  }
+      .then((data) => {
+        setCamDetails(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setLoading(false);
+  };
   //console.log(camDetails);
-   return ( 
-
+  return (
     <div>
-        <div className='head'><h1>Webcams</h1>
-        </div>      
-        {/*TODO: Modal */}
-           {modal && (
-            <VideoPlayer 
-              camDetails={camDetails} 
-              modal={modal} 
-              setModal={setModal}
-              loading={loading}
-              setLoading={setLoading}
-              />
-          )}
-        <div>
-          { loading ? 'Loading...' :
-          webcams?.webcams?.map((webcam, id) => (
-            <div key={id} className='webcams-list'>
-              <h3>{webcam.title}</h3>
-              {/* <h5>{webcam.status}</h5> */}
-              <button className='button-getLink' id={webcam.webcamId} onClick={handlerClick }>Get Link</button>
-            </div>
-          ))}
-          </div>
+      <div className="head">
+        <h1>Webcams</h1>
+      </div>
+      {/*TODO: Modal */}
+      {modal && (
+        <VideoPlayer
+          camDetails={camDetails}
+          modal={modal}
+          setModal={setModal}
+          loading={loading}
+          setLoading={setLoading}
+        />
+      )}
+      <div>
+        <h5 style={{ color: "chocolate" }}>
+          Sorry, This Page is Actually Under Construction
+        </h5>
+        {loading
+          ? "Loading..."
+          : webcams?.webcams?.map((webcam, id) => (
+              <div key={id} className="webcams-list">
+                <h3>{webcam.title}</h3>
+                {/* <h5>{webcam.status}</h5> */}
+                <button
+                  className="button-getLink"
+                  id={webcam.webcamId}
+                  onClick={handlerClick}
+                >
+                  <img src={link} alt="link" width={12} height={10} />
+                </button>
+              </div>
+            ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Webcams
+export default Webcams;
